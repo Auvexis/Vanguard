dd2ad7d06a092a78b9bbd22c1eb602c0bb3be481
André Simões <andre.simoesdutra23@gmail.com>
2026-02-23 11:49:47 -0300

    config: enable rabbitmq on Main

--- src/main/java/com/auvexis/vanguard/VanguardApplication.java
+++ src/main/java/com/auvexis/vanguard/VanguardApplication.java
@@ -1,5 +1,6 @@
 package com.auvexis.vanguard;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -15,6 +16,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableCaching
 @EnableScheduling
+@EnableRabbit
 public class VanguardApplication {
 
 	public static void main(String[] args) {
