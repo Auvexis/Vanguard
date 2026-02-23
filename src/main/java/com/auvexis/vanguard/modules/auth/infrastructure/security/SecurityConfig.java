a734168a1e9c0ef5ce8de920d68488ceeaae1597
André Simões <andre.simoesdutra23@gmail.com>
2026-02-23 13:31:58 -0300

    cors: update cors allowed origins

--- src/main/java/com/auvexis/vanguard/modules/auth/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/auvexis/vanguard/modules/auth/infrastructure/security/SecurityConfig.java
@@ -103,7 +103,7 @@ public class SecurityConfig {
         /**
          * Allow the configured client URL and localhost:3000 as a fallback/dev default
          */
-        configuration.setAllowedOrigins(Arrays.asList(clientUrl, "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList(clientUrl, "http://localhost:3000", "https://vanguard.auvexis.com", "vanguard.auvexis.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
